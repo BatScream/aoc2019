d334bbc77847dd43bda81a9e075ffb9d8270bf14
BatScream <clement.lloyd@gmail.com>
2019-12-19 18:48:34 +0530

    day 14-b

---
+++ 14-b.js
@@ -0,0 +1,80 @@
+function readReactions(){
+    const fs = require('fs');
+    const text = fs.readFileSync('input.txt','utf8');
+    const reactions = {};
+    text.split('\n').forEach(line => {
+        const arr = line.split(' => ');
+        const output = new Chemical(arr[1].trim().split(' ')[1], parseInt(arr[1].split(' ')[0]));
+        const inputs = [];
+        arr[0].split(', ').forEach(item => {
+            inputs.push(new Chemical(item.trim().split(' ')[1], parseInt(item.split(' ')[0])));
+        });
+        reactions[output.name] = {
+            quantity: output.quantity,
+            reactants: inputs
+        };
+    });
+    return reactions;
+}
+
+class Chemical{
+    constructor(name, quantity){
+        this.name = name;
+        this.quantity = quantity;
+    }
+}
+
+function calculate(reactions) {
+    let leftOvers = {};
+    Object.keys(reactions).forEach(key => leftOvers[key] = 0);
+    function start(chemical){
+        const multiplier = Math.ceil(chemical.quantity/reactions[chemical.name].quantity);
+        let consumedOres = 0;
+        reactions[chemical.name].reactants.forEach(reactant => {
+            const quantityRequired = reactant.quantity * multiplier;
+            if(reactant.name == 'ORE'){
+                consumedOres += quantityRequired;
+            }
+            else if (leftOvers[reactant.name] < quantityRequired){
+                consumedOres += start(new Chemical(reactant.name, quantityRequired - leftOvers[reactant.name]));
+            }
+            leftOvers[reactant.name] = leftOvers[reactant.name] - quantityRequired;
+        });
+        leftOvers[chemical.name] = (leftOvers[chemical.name] || 0) + (multiplier * reactions[chemical.name].quantity);
+        console.log(consumedOres);
+        return consumedOres;
+    }
+    return {start}
+}
+
+function findHighestAmtOfFuel(){
+    const calculator = calculate(reactions);
+    let oreRequiredForOneFuel = calculator.start(new Chemical("FUEL", 1));
+    let low = 1e12/oreRequiredForOneFuel;
+    let high = 10 * low;
+
+    while (calculator.start(new Chemical("FUEL", high)) < 1e12){
+        low = high
+        high = 10 * low
+    }
+    let mid = (low + high) / 2;
+    while (low < high - 1){
+        mid = (low + high) / 2
+        let oresRequiredForFuels = calculator.start(new Chemical("FUEL", mid));
+        if (oresRequiredForFuels < 1e12){
+            low = mid;
+        }
+        else if( oresRequiredForFuels > 1e12){
+            high = mid;
+        }
+        else break
+    }
+    return mid;
+}
+
+const reactions = readReactions();
+console.log(findHighestAmtOfFuel());
+
+
+
+
